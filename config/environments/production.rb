a65d2b2fec99d9ab9dd2f3b9a40b820a5b52526c
Steven Thomas <steven.thomas@shopify.com>
2014-07-22 23:01:34 -0400

    Another update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,7 @@ Sav::Application.configure do
     address:        ENV['MAILGUN_SMTP_SERVER'],
     user_name:      ENV['MAILGUN_SMTP_LOGIN'],
     password:       ENV['MAILGUN_SMTP_PASSWORD'],
-    domain:         ENV["APP_URL"],
+    domain:         'savproject.herokuapp.com',
     authentication: :plain
   }
 
