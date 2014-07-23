08e60c65b6bd908eebdb2c3a8ec9ab7283c545f1
Steven Thomas <steven.thomas@shopify.com>
2014-07-22 23:04:41 -0400

    Update default email

--- app/mailers/email_mailer.rb
+++ app/mailers/email_mailer.rb
@@ -1,9 +1,9 @@
 class EmailMailer < ActionMailer::Base
-  default to: "steve@onbluedot.com"
+  default to: "studentsagainstviolence@gmail.com"
 
   def contact_mailer(contact)
     @contact = contact
-    mail(from: contact.email, 
-         subject: 'New message from SAV project contact form')  
+    mail(from: contact.email,
+         subject: 'New message from SAV project contact form')
   end
 end
