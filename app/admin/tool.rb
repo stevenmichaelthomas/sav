562a2b106ac0e85919a6179c17139b5d4006a389
Steven Thomas <steven.thomas@shopify.com>
2014-07-26 15:13:12 -0400

    Update columns to display on Tools admin

--- app/admin/tool.rb
+++ app/admin/tool.rb
@@ -1,3 +1,11 @@
 ActiveAdmin.register Tool do
   permit_params :title, :summary, :url, :phone, :category_id, :address
+
+  index do
+    column :title
+    column :url
+    column "Category" do |tool|
+      tool.category.name
+    end
+  end
 end
