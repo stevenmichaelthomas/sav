dfcab77e287fec9228a1f513427fc139794495e0
Steven Thomas <steven.thomas@shopify.com>
2014-07-26 16:13:23 -0400

    Add featured to permitted params

--- app/admin/tool.rb
+++ app/admin/tool.rb
@@ -1,5 +1,6 @@
 ActiveAdmin.register Tool do
-  permit_params :title, :summary, :url, :phone, :category_id, :address
+  permit_params :title, :summary, :url, :phone, :category_id, :address,
+                :featured
 
   index do
     column :title
