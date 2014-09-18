15dc40cc4bd8569afcd9f887728a2513f189f8ab
Steven Thomas <steven.thomas@shopify.com>
2014-09-18 14:55:38 -0400

    Update SAV default email

--- app/mailers/email_mailer.rb
+++ app/mailers/email_mailer.rb
@@ -1,5 +1,5 @@
 class EmailMailer < ActionMailer::Base
-  default to: Rails.env.production? ? "studentsagainstviolence@gmail.com" : "steve@onbluedot.com"
+  default to: Rails.env.production? ? "studentsagainstviolencerru@gmail.com" : "steve@onbluedot.com"
 
   def contact_mailer(contact)
     @contact = contact
@@ -9,7 +9,7 @@ class EmailMailer < ActionMailer::Base
 
   def support_mailer(support)
     @support = support
-    mail(from: support.email.present? ? support.email : "studentsagainstviolence@gmail.com",
+    mail(from: support.email.present? ? support.email : "studentsagainstviolencerru@gmail.com",
          subject: 'New application from SAV project peer support form')
   end
 end
