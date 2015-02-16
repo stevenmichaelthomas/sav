8474e158b94c286f68dbd4339248613eff008e5b
Steven Thomas <steven.thomas@shopify.com>
2015-02-16 17:32:00 -0500

    Style event table

--- app/admin/event.rb
+++ app/admin/event.rb
@@ -1,5 +1,5 @@
 ActiveAdmin.register Event do
-  permit_params :name, :date, :location, :address
+  permit_params :name, :date, :location, :address, :description
 
   # See permitted parameters documentation:
   # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
