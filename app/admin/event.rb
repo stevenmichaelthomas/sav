297e671defa66e6d3db2009b0f0b8ecf46341719
Steven Thomas <steven.thomas@shopify.com>
2015-02-16 17:26:38 -0500

    Permit event params, update sass, stub description

--- app/admin/event.rb
+++ app/admin/event.rb
@@ -1,6 +1,6 @@
 ActiveAdmin.register Event do
+  permit_params :name, :date, :location, :address
 
-  
   # See permitted parameters documentation:
   # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
@@ -13,5 +13,5 @@ ActiveAdmin.register Event do
   #  permitted << :other if resource.something?
   #  permitted
   # end
-  
+
 end
